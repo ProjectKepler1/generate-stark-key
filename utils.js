8075e18c49b4451dc2f934253145005feb6ad4a3
Aung T. T. Kyaw <zatlite@gmail.com>
2022-10-20 16:12:51 +1100

    feat: methods to generate deterministic stark key
    [DX-1308]

---
+++ utils.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+function getEnv(name, defaultValue = undefined) {
+  const value = process.env[name];
+
+  if (value !== undefined) {
+    return value;
+  }
+  if (defaultValue !== undefined) {
+    return defaultValue;
+  }
+  throw new Error(`Environment variable '${name}' not set`);
+}
+
+export function requireEnvironmentVariable(key) {
+  const value = getEnv(key);
+  if (!value) {
+    throw new Error(`Please ensure a value exists for ${key}`);
+  }
+  return value;
+}
